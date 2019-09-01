1713b1b3cc0f1443f818a04d1a1163daf506170c
spicyboiledfish <327434855@qq.com>
2019-09-01 17:05:32 +0800

    chore: update travis ci

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,7 +4,7 @@ describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base');
     console.log(baseConfig);
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, '/Users/wangrui/workspace/tech/builder-webpack-rui/test/smoke/template/src/index/index.js');
-        assert.equal(baseConfig.entry.search, '/Users/wangrui/workspace/tech/builder-webpack-rui/test/smoke/template/src/search/index.js');
+        assert.equal(baseConfig.entry.index.indexOf('builder-webpack-rui/test/smoke/template/src/index/index.js') > -1, true);
+        assert.equal(baseConfig.entry.search.indexOf('builder-webpack-rui/test/smoke/template/src/search/index.js') > -1, true);
     })
 })
